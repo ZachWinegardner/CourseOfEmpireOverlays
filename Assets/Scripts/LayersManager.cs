76a6df7becfb1080b41e638149dadd531864a506
Zach Winegardner <winegardner.55@osu.edu>
2017-08-30 17:05:21 -0400

    Layers in Canvas and swapping

---
+++ Assets/Scripts/LayersManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayersManager : MonoBehaviour {
+
+	public GameObject[] paintingLayers; 
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LayerButtonPressed (int index){
+
+		if (paintingLayers [index].transform.GetSiblingIndex() != 4) {
+			
+			paintingLayers [index].transform.SetSiblingIndex (4); 
+		}
+	}
+}
